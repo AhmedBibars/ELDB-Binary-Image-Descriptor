8060f42654843e11e2caadbb55d367b35d9cb11c
AhmedBibars <42313457+AhmedBibars@users.noreply.github.com>
2018-10-08 15:58:11 +0200

    Update Demo_SurfersParadise.m

--- Demo_SurfersParadise.m
+++ Demo_SurfersParadise.m
@@ -1,7 +1,7 @@
 %%%%%%%%%%%%%%%%%%%%%%%%%Parameters%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
 DatabaseVideoPath='G:\surfars paradise\Day_Modified.avi';
 QuaryVideoPath='G:\surfars paradise\Night_Modified.avi';
-ImageSize=64;    %redused image size
+ImageSize=64;    %reduced image size
 CoparisonsPerPair=3;       % 3 bits generated for each cell-pair comparison, 5 incase of ELDB2 
 SelectedComparisonsNum=4000;       % number of randomly selected cell-pairs
 load GroundTruth_SurfersParadise;  % groundtruth quary/database equivelant frames, to compare our results with it.
